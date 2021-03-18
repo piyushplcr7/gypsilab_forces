8616d5e3a140161f33e2d9d0209194f6f71baa1b
MartinA <maverseng@ethz.ch>
2021-03-18 21:54:03 +0100

    cleaning code structure

--- Nikolas.m
+++
@@ -1,6 +0,0 @@
-clc;
-
-disp(char([087 097 115 032 108 097 117 102 116 032 117 110 100 032 119 097 115 032 119 105 114 100 032 103 101 119 111 114 102 101 110 063 013 010]));
-addpath(genpath(pwd));
-
-run nonRegressionTest/WeightedBIE/Preconditioning/nrtPrecHelmholtzCurves.m;
--- RalfTeamTests/miscellaneous/fluxTest.m
+++
@@ -1,8 +0,0 @@
-m = mshCube(20,[1 1 1]);
-Vh = fem(m,'RWG');
-uh = ones(Vh.ndof,1);
-traceU = normalTrace_RT(Vh)*uh;
-plot(dm,traceU)
-hold on
-plotNrm(dm,'r')
-pltNaturalNrm(dm,'g');
