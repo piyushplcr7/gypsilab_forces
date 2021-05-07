7b8c345eccfa50ac738c0b6ecc251d73bc7a7d64
Martin A <martin.averseng@gmail.com>
2021-05-07 10:46:24 +0200

    scal3D

--- Gypsi_files/openMsh/mshNdvolume.m
+++ Gypsi_files/openMsh/mshNdvolume.m
@@ -69,3 +69,13 @@ else
     error('mshNdvolume.m : unavailable case')
 end
 end
+
+function [r] = scal3D(X,Y)
+% X,Y are Nx3 array containing vector coordinates. 
+% X and/or Y may be 1x3.
+
+r = X(:,1).*Y(:,1) + X(:,2).*Y(:,2) + X(:,3).*Y(:,3);
+
+end
+
+
