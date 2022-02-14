30226d3bb15b33a5317aeb34c67af7f1996bf413
Piyush <ppanchal@student.ethz.ch>
2022-02-14 07:43:16 +0100

    BEM evaluation using double layer

--- Piyush's codes/3D BEM Sauter Schwab/Torque computations/fixed_charge/compute_bem_forces_gypsi.m
+++ Piyush's codes/3D BEM Sauter Schwab/Torque computations/fixed_charge/compute_bem_forces_gypsi.m
@@ -1,23 +1,21 @@
-function torques = compute_bem_forces_gypsi(mesh,R,Psi)
+% Function to compute BEM  forces using Gypsi implementation. 
+function out = compute_bem_forces_gypsi(mesh,Psi,nu)
 
+    % Force computation using the double layer. One arg is Psi, other is
+    % Psi \nu \cdot n
     S0_Gamma = fem(mesh,'P0');
+    normals = mesh.nrm;
+    dofs = S0_Gamma.dof;
+    Gamma = dom(mesh,3);
 
-    Nux = @(X) (vecnorm(X,2,2)<R).* ones(size(X,1),1)*[1 0 0];
-    Nuy = @(X) (vecnorm(X,2,2)<R).* ones(size(X,1),1)*[0 1 0];
-    Nuz = @(X) (vecnorm(X,2,2)<R).* ones(size(X,1),1)*[0 0 1];
+    GradG = cell(3,1);
+    GradG{1} = @(X,Y)femGreenKernel(X,Y,'grady[1/r]1',0);
+    GradG{2} = @(X,Y)femGreenKernel(X,Y,'grady[1/r]2',0);
+    GradG{3} = @(X,Y)femGreenKernel(X,Y,'grady[1/r]3',0);
+    K = 1/(4*pi)*integral(Gamma,Gamma,S0_Gamma,GradG,ntimes(S0_Gamma));
+    K = K +1/(4*pi)*regularize(Gamma,Gamma,S0_Gamma,'grady[1/r]',ntimes(S0_Gamma));
 
-    % Kernels
-    kernelx = @(x,y,z) sum(z.*(Nux(x) - Nux(y)), 2)./(vecnorm(z,2,2).^3)/ (4*pi);
-    kernely = @(x,y,z) sum(z.*(Nuy(x) - Nuy(y)), 2)./(vecnorm(z,2,2).^3)/ (4*pi);
-    kernelz = @(x,y,z) sum(z.*(Nuz(x) - Nuz(y)), 2)./(vecnorm(z,2,2).^3)/ (4*pi);
-
-    t2matx = panel_oriented_assembly(mesh,kernelx,S0_Gamma,S0_Gamma);
-    forcex = 0.5 * dot(Psi,t2matx*Psi);
-    t2maty = panel_oriented_assembly(mesh,kernely,S0_Gamma,S0_Gamma);
-    forcey = 0.5 * dot(Psi,t2maty*Psi);
-    t2matz = panel_oriented_assembly(mesh,kernelz,S0_Gamma,S0_Gamma);
-    forcez = 0.5 * dot(Psi,t2matz*Psi);
-
-    torques = [forcex forcey forcez];
-
+    % Right vectors for forces
+    Psi_nu = Psi.* dot(nu(dofs),normals,2);
+    out = 2*dot(Psi_nu,K*Psi);
 end
