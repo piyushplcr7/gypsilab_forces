074d975c3c8deaa4966928f2d8b46fee60e9a0e5
ppanchal <piyush.panchal@sam.math.ethz.ch>
2023-07-05 13:10:45 +0200

    Added parallelization to permanent magnet + SP + BIE computation

--- Piyush's codes/Magnetostatics/Permanent Magnet/PermanentMagnetShapeDerivativeBIESP.m
+++ Piyush's codes/Magnetostatics/Permanent Magnet/PermanentMagnetShapeDerivativeBIESP.m
@@ -37,30 +37,48 @@ function sd = PermanentMagnetShapeDerivativeBIESP(Gamma,Tdu,Tnu,J,omega_src,Vel,
     % Kernel gradxG.vel(x) + gradyG.vel(y), z:= y-x
     kernelold = @(x,y,z) dot(z,Vel(x) - Vel(y), 2)./(vecnorm(z,2,2).^3)/ (4*pi);
 
-    kerneloldmat_P0_P0 = panel_assembly(bndmesh,kernelold,P0,P0,ii(:),jj(:));
-    kerneloldmat_nxgradP1_nxgradP1 = panel_assembly(bndmesh,kernelold,nxgradP1,nxgradP1,ii(:),jj(:));
+    kernelintegrable = @(x,y,z) 3/(4*pi)* dot(z,Vel(y) - Vel(x),2) .*z ./vecnorm(z,2,2).^5;
 
-    % Partial derivative of bv(psi,psi)
-    dbv_ds = Tnu' * kerneloldmat_P0_P0 * Tnu;
+    combkernel = @(x,y,z) 1/(4*pi) * ( -[ dot(DVel{1}(y),z,2) dot(DVel{2}(y),z,2) dot(DVel{3}(y),z,2) ] + Vel(y) - Vel(x) )./vecnorm(z,2,2).^3;
+
+    KV = @(x,y,z) 1./vecnorm(z,2,2)/4./pi;
+
+    euler = parcluster('local');
+    euler.NumWorkers = 5;
+    saveProfile(euler);
+
+    pool = euler.parpool(5);
+
+    spmd
+        if spmdIndex==1
+            kerneloldmat_P0_P0 = panel_assembly(bndmesh,kernelold,P0,P0,ii(:),jj(:));
+            % Partial derivative of bv(psi,psi)
+            dbv_ds = Tnu' * kerneloldmat_P0_P0 * Tnu;
+        
+        elseif spmdIndex==2
+            kerneloldmat_nxgradP1_nxgradP1 = panel_assembly(bndmesh,kernelold,nxgradP1,nxgradP1,ii(:),jj(:));
+        
+        elseif spmdIndex==3
+            kernelintegrablemat = panel_assembly(bndmesh,kernelintegrable,ntimes(P1),P0,ii(:),jj(:));
+
+        elseif spmdIndex==4
+            % Combination kernel that cancels singularity
+            combkernelmat = panel_assembly(bndmesh,combkernel,ntimes(P1),P0,ii(:),jj(:));
+
+        elseif spmdIndex==5
+            SL_Dvelnxgrad_nxgrad = panel_assembly_shape_derivative(bndmesh,KV,nxgradP1,nxgradP1,ii(:),jj(:),Vel,DVel);
+        end
+    end
 
     % partial derivative of bk(g,psi)
     divVelg = divVel.*g;
     divVelg_coeffs = proj(divVelg,Gamma,P1);
     Kmat = double_layer_laplace(Gamma,P0,P1);
 
-    kernelintegrable = @(x,y,z) 3/(4*pi)* dot(z,Vel(y) - Vel(x),2) .*z ./vecnorm(z,2,2).^5;
-    kernelintegrablemat = panel_assembly(bndmesh,kernelintegrable,ntimes(P1),P0,ii(:),jj(:));
-
-    % Combination kernel that cancels singularity
-    combkernel = @(x,y,z) 1/(4*pi) * ( -[ dot(DVel{1}(y),z,2) dot(DVel{2}(y),z,2) dot(DVel{3}(y),z,2) ] + Vel(y) - Vel(x) )./vecnorm(z,2,2).^3;
-    combkernelmat = panel_assembly(bndmesh,combkernel,ntimes(P1),P0,ii(:),jj(:));
-
-    dbk_ds = Tnu' * Kmat * divVelg_coeffs + Tnu' * (kernelintegrablemat -combkernelmat) * Tdu;
+    dbk_ds = Tnu' * Kmat * divVelg_coeffs + Tnu' * (kernelintegrablemat{3} -combkernelmat{4}) * Tdu;
 
     % Partial derivative of bw(g,g)
-    KV = @(x,y,z) 1./vecnorm(z,2,2)/4./pi;
-    SL_Dvelnxgrad_nxgrad = panel_assembly_shape_derivative(bndmesh,KV,nxgradP1,nxgradP1,ii(:),jj(:),Vel,DVel);
-    dbw_ds = Tdu' * ( kerneloldmat_nxgradP1_nxgradP1 + 2 * SL_Dvelnxgrad_nxgrad) * Tdu;
+    dbw_ds = Tdu' * ( kerneloldmat_nxgradP1_nxgradP1{2} + 2 * SL_Dvelnxgrad_nxgrad{5}) * Tdu;
 
     %% Linear form
     Mvals = M(X);
@@ -68,18 +86,19 @@ function sd = PermanentMagnetShapeDerivativeBIESP(Gamma,Tdu,Tnu,J,omega_src,Vel,
 
     lambda_coeffs = proj(Mdotn,Gamma,P0);
 
-    l1 = Tnu' * kerneloldmat_P0_P0 * lambda_coeffs;
+    l1 = Tnu' * kerneloldmat_P0_P0{1} * lambda_coeffs;
 
-    l2 = lambda_coeffs' * Kmat * divVelg_coeffs + lambda_coeffs' * (kernelintegrablemat -combkernelmat) * Tdu;
+    l2 = lambda_coeffs' * Kmat * divVelg_coeffs + lambda_coeffs' * (kernelintegrablemat{3} -combkernelmat{4}) * Tdu;
     l2 = -l2;
 
     %% Remaining terms
 
-    r1 = -mu0/2 * (lambda_coeffs' * kerneloldmat_P0_P0 * lambda_coeffs);
+    r1 = -mu0/2 * (lambda_coeffs' * kerneloldmat_P0_P0{1} * lambda_coeffs);
     r2 = mu0 * sum(W.* Mdotn .* dot(Vels,HJ,2),1);
 
 
-    sd = mu0/2 * (-2*dbv_ds + 4 * dbk_ds +2 * dbw_ds)+ mu0  * (l1 + l2) +r1 + r2;
+    sd = mu0/2 * (-2*dbv_ds{1} + 4 * dbk_ds +2 * dbw_ds)+ mu0  * (l1 + l2) +r1 + r2;
     
+    pool.delete();
 
 end
