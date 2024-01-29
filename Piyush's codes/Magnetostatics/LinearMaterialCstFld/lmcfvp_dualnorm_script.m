e8c0dc21ea14fc4d60538f29c8726fc001d77f4d
ppanchal <piyush.panchal@sam.math.ethz.ch>
2024-01-29 13:17:53 +0100

    lmcfvp dualnorm script

---
+++ Piyush's codes/Magnetostatics/LinearMaterialCstFld/lmcfvp_dualnorm_script.m
@@ -0,0 +1,9 @@
+% Forces and torques script
+gpuDevice(1);
+delete(gcp('nocreate'));
+addpath(genpath("../../../"));
+
+LMCFVP_dualnorm(@getMeshSphere,5:11);
+LMCFVP_dualnorm(@getMeshCube,5:11);
+LMCFVP_dualnorm(@getMeshCuboid5,5:11);
+LMCFVP_dualnorm(@getMeshTetraNew,7:11);
