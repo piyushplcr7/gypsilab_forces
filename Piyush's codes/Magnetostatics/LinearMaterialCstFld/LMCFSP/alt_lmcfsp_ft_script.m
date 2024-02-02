acedb538923c46aa611865453430b5f402f800c1
ppanchal <ppanchal@student.ethz.ch>
2024-02-02 10:18:04 +0100

    lmcfsp alt ft data ada

--- Piyush's codes/Magnetostatics/LinearMaterialCstFld/LMCFSP/alt_lmcfsp_ft_script.m
+++ Piyush's codes/Magnetostatics/LinearMaterialCstFld/LMCFSP/alt_lmcfsp_ft_script.m
@@ -3,7 +3,7 @@
 delete(gcp('nocreate'));
 addpath(genpath("../../../../"));
 
-LMCFSP_forcesNtorquesAlt(@getMeshSphere,5:11);
-LMCFSP_forcesNtorquesAlt(@getMeshCube,5:11);
-LMCFSP_forcesNtorquesAlt(@getMeshCuboid5,5:11);
+%LMCFSP_forcesNtorquesAlt(@getMeshSphere,5:11);
+%LMCFSP_forcesNtorquesAlt(@getMeshCube,5:11);
+%LMCFSP_forcesNtorquesAlt(@getMeshCuboid5,5:11);
 LMCFSP_forcesNtorquesAlt(@getMeshTetraNew,7:11);
