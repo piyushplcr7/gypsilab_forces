9c43f7a533f297be50fa3f2ce4de58d419e72c80
ppanchal <piyush.panchal@sam.math.ethz.ch>
2024-02-12 22:26:30 +0100

    PMSP shape derivative CUDA removed

--- Piyush's codes/Magnetostatics/Permanent Magnet/ShapeDerivatives/PermanentMagnetShapeDerivativeBIESP.m
+++ Piyush's codes/Magnetostatics/Permanent Magnet/ShapeDerivatives/PermanentMagnetShapeDerivativeBIESP.m
@@ -49,106 +49,106 @@ function sd = PermanentMagnetShapeDerivativeBIESP(Gamma,Tdu,Tnu,J,omega_src,Vel,
     lambda_coeffs = proj(Mdotn,Gamma,P0);
     
     %% Dispatching SS computations to GPU 
-    [~,elt2dof] = P1.dof;
-    % Creating the interaction matrix
-    elts = 1:bndmesh.nelt;
-    elts = elts';
-    elts = repelem(elts,3);
-    vtcs = reshape(bndmesh.elt',[bndmesh.nelt*3 1]);
-    
-    Eltmat = sparse(elts,vtcs,ones(size(vtcs)),bndmesh.nelt,bndmesh.nvtx);
-    
-    Intmat = Eltmat * Eltmat';
-    [I0,J0] = find(Intmat == 0);
-    [I1,J1] = find(Intmat == 1);
-    [I2,J2] = find(Intmat == 2);
-    [I3,J3] = find(Intmat == 3);
-    Ivec = [I0;I1;I2;I3]-1;
-    Ivec = cast(Ivec,'int32');
-    Jvec = [J0;J1;J2;J3]-1;
-    Jvec = cast(Jvec,'int32');
-    relation = [0*ones(size(I0,1),1);...
-                1*ones(size(I1,1),1);...
-                2*ones(size(I2,1),1);...
-                3*ones(size(I3,1),1)];
-    relation = cast(relation,'int32');
-
-    % PTX and CUDA files
-    ptxFilePath = 'PermanentMagnetShapeDerivativeBIESP_CUDA.ptx';
-    cuFilePath = 'PermanentMagnetShapeDerivativeBIESP_CUDA.cu';
-
-    % Quadrature to be passed to the GPU
-    order = 5;
-    [X, W] = quad4D(order);
-    %[X, W] = quad4DfromSquare(@rule30);
-
-    % Kernel Object
-    kernel = parallel.gpu.CUDAKernel(ptxFilePath, cuFilePath);
-    gridDim = [150, 1, 1];
-    blockDim = [32, 1, 1]; % 32 is the SIMD Width or wrap size
-    Nthreads = prod(blockDim) * prod(gridDim);
-    kernel.GridSize = gridDim;
-    kernel.ThreadBlockSize = blockDim; 
-
-    % Transferring Interactions and Relations to GPU
-    Ivec_gpu = gpuArray(Ivec);
-    Jvec_gpu = gpuArray(Jvec);
-    relation_gpu = gpuArray(relation);
-
-    % Transferring Quadrature to GPU
-    W0_gpu = gpuArray(W{4});
-    W1_gpu = gpuArray(W{3});
-    W2_gpu = gpuArray(W{2});
-    W3_gpu = gpuArray(W{1});
-    X0 = X{4}; X0(:,1) = X0(:,1)-X0(:,2); X0(:,3) = X0(:,3)-X0(:,4);
-    X0_gpu = gpuArray(X0');
-    X1 = X{3}; X1(:,1) = X1(:,1)-X1(:,2); X1(:,3) = X1(:,3)-X1(:,4);
-    X1_gpu = gpuArray(X1');
-    X2 = X{2}; X2(:,1) = X2(:,1)-X2(:,2); X2(:,3) = X2(:,3)-X2(:,4);
-    X2_gpu = gpuArray(X2');
-    X3 = X{1}; X3(:,1) = X3(:,1)-X3(:,2); X3(:,3) = X3(:,3)-X3(:,4);
-    X3_gpu = gpuArray(X3');
-
-    % Output variables GPU
-    dbv_ds_gpu = gpuArray.zeros(1,1);
-    dbw_ds_gpu = gpuArray.zeros(1,1);
-    dbk_ds_reduced_gpu = gpuArray.zeros(1,1); % excludes the Kmat part which is computed by the CPU
-    l1_gpu = gpuArray.zeros(1,1);
-    l2_gpu = gpuArray.zeros(1,1);
-    r1_gpu = gpuArray.zeros(1,1);
-    shapeDerivative_gpu = gpuArray.zeros(1,1);
-
-    % More input variables to GPU
-    Tdu_gpu = gpuArray(Tdu);
-    Tnu_gpu = gpuArray(Tnu);
-    lambda_coeffs_gpu = gpuArray(full(lambda_coeffs));
-    Elements = cast(bndmesh.elt-1,'int32');
-    Elements_gpu = gpuArray(Elements');
-    Vertices_gpu = gpuArray(bndmesh.vtx');
-    Normals_gpu = gpuArray(bndmesh.nrm');
-    Areas_gpu = gpuArray(bndmesh.ndv);
-    zeroIdxelt2dof = cast(elt2dof-1,'int32');
-    elt2dof_gpu = gpuArray(zeroIdxelt2dof');
-    TrialSpace_gpu = 0;
-    TestSpace_gpu = 0;
-    TrialOperator_gpu = 0;
-    TestOperator_gpu = 0;
-
-    % Launching kernel
-    [dbv_ds_gpu,dbw_ds_gpu,dbk_ds_reduced_gpu,l1_gpu,l2_gpu,r1_gpu,shapeDerivative_gpu]= feval(kernel,...
-    0,0,bndmesh.nelt,bndmesh.nvtx,bndmesh.nelt^2,...
-    Nthreads,Ivec_gpu,Jvec_gpu,relation_gpu,...
-    W0_gpu,X0_gpu,size(X0,1),...
-    W1_gpu,X1_gpu,size(X1,1),...
-    W2_gpu,X2_gpu,size(X2,1),...
-    W3_gpu,X3_gpu,size(X3,1),...
-    dbv_ds_gpu,dbw_ds_gpu,dbk_ds_reduced_gpu,l1_gpu,l2_gpu,r1_gpu,...
-    shapeDerivative_gpu,...
-    Tdu_gpu, Tnu_gpu,lambda_coeffs_gpu,mu0,...
-    Elements_gpu,Vertices_gpu,Normals_gpu,Areas_gpu,...
-    elt2dof_gpu,elt2dof_gpu,...
-    TrialSpace_gpu,TestSpace_gpu,TrialOperator_gpu,TestOperator_gpu,...
-    size(P1.rsf,1),size(P1.rsf,1)); 
+    % [~,elt2dof] = P1.dof;
+    % % Creating the interaction matrix
+    % elts = 1:bndmesh.nelt;
+    % elts = elts';
+    % elts = repelem(elts,3);
+    % vtcs = reshape(bndmesh.elt',[bndmesh.nelt*3 1]);
+    % 
+    % Eltmat = sparse(elts,vtcs,ones(size(vtcs)),bndmesh.nelt,bndmesh.nvtx);
+    % 
+    % Intmat = Eltmat * Eltmat';
+    % [I0,J0] = find(Intmat == 0);
+    % [I1,J1] = find(Intmat == 1);
+    % [I2,J2] = find(Intmat == 2);
+    % [I3,J3] = find(Intmat == 3);
+    % Ivec = [I0;I1;I2;I3]-1;
+    % Ivec = cast(Ivec,'int32');
+    % Jvec = [J0;J1;J2;J3]-1;
+    % Jvec = cast(Jvec,'int32');
+    % relation = [0*ones(size(I0,1),1);...
+    %             1*ones(size(I1,1),1);...
+    %             2*ones(size(I2,1),1);...
+    %             3*ones(size(I3,1),1)];
+    % relation = cast(relation,'int32');
+    % 
+    % % PTX and CUDA files
+    % ptxFilePath = 'PermanentMagnetShapeDerivativeBIESP_CUDA.ptx';
+    % cuFilePath = 'PermanentMagnetShapeDerivativeBIESP_CUDA.cu';
+    % 
+    % % Quadrature to be passed to the GPU
+    % order = 5;
+    % [X, W] = quad4D(order);
+    % %[X, W] = quad4DfromSquare(@rule30);
+    % 
+    % % Kernel Object
+    % kernel = parallel.gpu.CUDAKernel(ptxFilePath, cuFilePath);
+    % gridDim = [150, 1, 1];
+    % blockDim = [32, 1, 1]; % 32 is the SIMD Width or wrap size
+    % Nthreads = prod(blockDim) * prod(gridDim);
+    % kernel.GridSize = gridDim;
+    % kernel.ThreadBlockSize = blockDim; 
+    % 
+    % % Transferring Interactions and Relations to GPU
+    % Ivec_gpu = gpuArray(Ivec);
+    % Jvec_gpu = gpuArray(Jvec);
+    % relation_gpu = gpuArray(relation);
+    % 
+    % % Transferring Quadrature to GPU
+    % W0_gpu = gpuArray(W{4});
+    % W1_gpu = gpuArray(W{3});
+    % W2_gpu = gpuArray(W{2});
+    % W3_gpu = gpuArray(W{1});
+    % X0 = X{4}; X0(:,1) = X0(:,1)-X0(:,2); X0(:,3) = X0(:,3)-X0(:,4);
+    % X0_gpu = gpuArray(X0');
+    % X1 = X{3}; X1(:,1) = X1(:,1)-X1(:,2); X1(:,3) = X1(:,3)-X1(:,4);
+    % X1_gpu = gpuArray(X1');
+    % X2 = X{2}; X2(:,1) = X2(:,1)-X2(:,2); X2(:,3) = X2(:,3)-X2(:,4);
+    % X2_gpu = gpuArray(X2');
+    % X3 = X{1}; X3(:,1) = X3(:,1)-X3(:,2); X3(:,3) = X3(:,3)-X3(:,4);
+    % X3_gpu = gpuArray(X3');
+    % 
+    % % Output variables GPU
+    % dbv_ds_gpu = gpuArray.zeros(1,1);
+    % dbw_ds_gpu = gpuArray.zeros(1,1);
+    % dbk_ds_reduced_gpu = gpuArray.zeros(1,1); % excludes the Kmat part which is computed by the CPU
+    % l1_gpu = gpuArray.zeros(1,1);
+    % l2_gpu = gpuArray.zeros(1,1);
+    % r1_gpu = gpuArray.zeros(1,1);
+    % shapeDerivative_gpu = gpuArray.zeros(1,1);
+    % 
+    % % More input variables to GPU
+    % Tdu_gpu = gpuArray(Tdu);
+    % Tnu_gpu = gpuArray(Tnu);
+    % lambda_coeffs_gpu = gpuArray(full(lambda_coeffs));
+    % Elements = cast(bndmesh.elt-1,'int32');
+    % Elements_gpu = gpuArray(Elements');
+    % Vertices_gpu = gpuArray(bndmesh.vtx');
+    % Normals_gpu = gpuArray(bndmesh.nrm');
+    % Areas_gpu = gpuArray(bndmesh.ndv);
+    % zeroIdxelt2dof = cast(elt2dof-1,'int32');
+    % elt2dof_gpu = gpuArray(zeroIdxelt2dof');
+    % TrialSpace_gpu = 0;
+    % TestSpace_gpu = 0;
+    % TrialOperator_gpu = 0;
+    % TestOperator_gpu = 0;
+    % 
+    % % Launching kernel
+    % [dbv_ds_gpu,dbw_ds_gpu,dbk_ds_reduced_gpu,l1_gpu,l2_gpu,r1_gpu,shapeDerivative_gpu]= feval(kernel,...
+    % 0,0,bndmesh.nelt,bndmesh.nvtx,bndmesh.nelt^2,...
+    % Nthreads,Ivec_gpu,Jvec_gpu,relation_gpu,...
+    % W0_gpu,X0_gpu,size(X0,1),...
+    % W1_gpu,X1_gpu,size(X1,1),...
+    % W2_gpu,X2_gpu,size(X2,1),...
+    % W3_gpu,X3_gpu,size(X3,1),...
+    % dbv_ds_gpu,dbw_ds_gpu,dbk_ds_reduced_gpu,l1_gpu,l2_gpu,r1_gpu,...
+    % shapeDerivative_gpu,...
+    % Tdu_gpu, Tnu_gpu,lambda_coeffs_gpu,mu0,...
+    % Elements_gpu,Vertices_gpu,Normals_gpu,Areas_gpu,...
+    % elt2dof_gpu,elt2dof_gpu,...
+    % TrialSpace_gpu,TestSpace_gpu,TrialOperator_gpu,TestOperator_gpu,...
+    % size(P1.rsf,1),size(P1.rsf,1)); 
 
 
     %% CPU
@@ -204,8 +204,8 @@ function sd = PermanentMagnetShapeDerivativeBIESP(Gamma,Tdu,Tnu,J,omega_src,Vel,
     r1 = -mu0/2 * (lambda_coeffs' * kerneloldmat_P0_P0{1} * lambda_coeffs);
     r2 = mu0 * sum(Wgypsi.* Mdotn .* dot(Vels,HJ,2),1);
 
-    sd_reduced = mu0/2 * (-2*dbv_ds{1} + 4 * dbk_ds_reduced +2 * dbw_ds)+ mu0  * (l1 + l2reduced) +r1;
-    sd_comb = shapeDerivative_gpu + 2 * mu0 * Tnu' * Kmat * divVelg_coeffs + r2 - mu0 * lambda_coeffs' * Kmat * divVelg_coeffs;
+    % sd_reduced = mu0/2 * (-2*dbv_ds{1} + 4 * dbk_ds_reduced +2 * dbw_ds)+ mu0  * (l1 + l2reduced) +r1;
+    % sd_comb = shapeDerivative_gpu + 2 * mu0 * Tnu' * Kmat * divVelg_coeffs + r2 - mu0 * lambda_coeffs' * Kmat * divVelg_coeffs;
     sd = mu0/2 * (-2*dbv_ds{1} + 4 * dbk_ds +2 * dbw_ds)+ mu0  * (l1 + l2) +r1 + r2;
     
     pool.delete();
