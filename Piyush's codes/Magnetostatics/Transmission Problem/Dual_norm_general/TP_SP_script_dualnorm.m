50b5b636747eb350381b101f546d99de650c033e
ppanchal <ppanchal@student.ethz.ch>
2024-03-21 14:30:24 +0100

    data commit on ada, mu=4,mu0=2 with fixed solver and mesh

--- Piyush's codes/Magnetostatics/Transmission Problem/Dual_norm_general/TP_SP_script_dualnorm.m
+++ Piyush's codes/Magnetostatics/Transmission Problem/Dual_norm_general/TP_SP_script_dualnorm.m
@@ -2,11 +2,11 @@
 delete(gcp('nocreate'));
 addpath(genpath("../../../../"));
 
-gpuDevice(2);
+gpuDevice(3);
 valsrest = 5:12;
 valstetra = 7:11;
 
- TP_SP_dualnorm(@getMeshCuboid5,valsrest);
+% TP_SP_dualnorm(@getMeshCuboid5,valsrest);
 % TP_SP_dualnorm(@getMeshCube,valsrest);
 % TP_SP_dualnorm(@getMeshSphere,valsrest);
-% TP_SP_dualnorm(@getMeshTetraNew,valstetra);
+ TP_SP_dualnorm(@getMeshTetraNew,valstetra);
